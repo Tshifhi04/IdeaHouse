c45dbe232def6f084584deacfc5032cc47084e7c
Tshifhiwa Nevhuhulwi <vhuhulwit@gmail.com>
2023-05-14 22:58:05 +0200

    Add project files.

---
+++ IdeaHouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IdeaHouse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdeaHouse.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Idea> Ideas { get; set; }
+
+    }
+}
---
+++ IdeaHouse/Models/Idea.cs
@@ -0,0 +1,14 @@
+﻿namespace IdeaHouse.Models
+{
+    public class Idea
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Rating { get; set; }
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+      //  public int CategoryId { get; set; }
+
+    }
+}
